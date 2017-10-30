1cf3ce971d90fcd6eef1e338f6f1fc29b27f7854
Illusion <gunboy83@gmail.com>
2017-10-30 19:36:29 +0900

    minor

--- flask-practice1.py
+++ flask-practice1.py
@@ -3,7 +3,8 @@ import os, glob
 
 app = Flask(__name__)
 
-REPORT_DIRECTORY_PATH = '/Users/Illusion/IdeaProjects/flask-practice1/static/gallery/'
+#REPORT_DIRECTORY_PATH = '/Users/Illusion/IdeaProjects/flask-practice1/static/gallery/'
+REPORT_DIRECTORY_PATH = '/Users/Illusion/PycharmProjects/flask-gallery/static/gallery/'
 
 @app.route('/')
 def hello_world():
@@ -24,9 +25,23 @@ def show_images_of_the_dir(directory_name):
     else:
         return 'path does not exist'
 
-@app.route('/view_image_columns/')
+@app.route('/view_image_columns/<directory_name>')
 # show the images in columns
-def show_images_in_columns():
+def show_images_in_columns(directory_name):
+
+    # get the names of sub-directories
+    print 'directory_name = ', directory_name
+
+    img_path = os.path.join(REPORT_DIRECTORY_PATH, directory_name)
+    if os.path.exists(img_path):
+        print 'the path exists: ', img_path
+        os.chdir(img_path)
+        img_list = glob.glob('*.jpg')
+        print img_list
+        return render_template('view_image_columns.html', dir_name=directory_name, img_list=img_list)
+    else:
+        return 'path does not exist'
+
     return render_template('view_image_columns.html')
 
 if __name__ == '__main__':
